7477d00ea2d72ee472d4cd7c68f243f3dc389a6a
charles66820 <charles.goedefroit@gmail.com>
2019-04-10 11:51:18 +0200

    json request

--- app/src/main/java/fr/magicorp/andexp/api.java
+++ app/src/main/java/fr/magicorp/andexp/api.java
@@ -3,6 +3,16 @@ package fr.magicorp.andexp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class api extends AppCompatActivity {
 
@@ -17,6 +27,31 @@ public class api extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.api_activity);
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://api.magicorp.fr/batrenis/v1/servers";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        textView.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        textView.setText("Response: " + error.toString());
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+
     }
 
     //http://api.dev.magicorp.net:8080/batrenis/v1/servers
